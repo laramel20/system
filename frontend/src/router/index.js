8bc8c2af78eb65dfa3be75b867680b5ac6926ff2
laramel20 <hidalgolaramelissa2003@gmail.com>
2023-11-19 23:48:49 +1200

    main

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -21,6 +21,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "admin" */ '../views/AdminView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
   }
 ]
 
