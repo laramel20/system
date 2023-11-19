684deb2bad8f909e3e46466a6333ebcc1ecff464
laramel20 <hidalgolaramelissa2003@gmail.com>
2023-11-19 20:26:24 +1200

    main

---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,126 @@
+<template>
+  <body>
+    <div class="container-scroller">
+      <div class="row p-0 m-0 proBanner" id="proBanner">
+        <div class="col-md-12 p-0 m-0">
+          <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+          </div>
+        </div>
+      </div>
+      <!-- partial:partials/_navbar.html -->
+      <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+          <a class="navbar-brand brand-logo" href="index.html"><img :src="('../assets/images/logo.svg')"/></a>
+          <a class="navbar-brand brand-logo-mini" href="index.html"><img :src="('../assets/images/logo.mini.svg')"/></a>
+        </div>
+        <div class="navbar-menu-wrapper d-flex align-items-stretch">
+          <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="mdi mdi-menu"></span>
+          </button>
+          <div class="search-field d-none d-md-block">
+            <form class="d-flex align-items-center h-100" action="#">
+              <div class="input-group">
+                <div class="input-group-prepend bg-transparent">
+                  <i class="input-group-text border-0 mdi mdi-magnify"></i>
+                </div>
+                <input type="text" class="form-control bg-transparent border-0" placeholder="Search projects">
+              </div>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-profile">
+              <a href="#" class="nav-link">
+                <div class="nav-profile-image">
+                  <img :src="('../assets/images/faces/face1.jpg')">
+                  <span class="login-status online"></span>
+                  <!--change to offline or busy as needed-->
+                </div>
+                <div class="nav-profile-text d-flex flex-column">
+                  <span class="font-weight-bold mb-2">Admin</span>
+                  <span class="text-secondary text-small">Store Owner</span>
+                </div>
+                <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/admin">
+                <span class="menu-title">Dashboard</span>
+                <i class="mdi mdi-home menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">Inventory</span>
+                <i class="menu-arrow"></i>
+                <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+              </a>
+
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/icons/mdi.html">
+                <span class="menu-title">Product Management</span>
+                <i class="mdi mdi-contacts menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/forms/basic_elements.html">
+                <span class="menu-title">Sales and Tracking</span>
+                <i class="mdi mdi-format-list-bulleted menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/charts/chartjs.html">
+                <span class="menu-title">Reporting and Analysis</span>
+                <i class="mdi mdi-chart-bar menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pages/tables/basic-table.html">
+                <span class="menu-title">Feedback and Recommendations</span>
+                <i class="mdi mdi-table-large menu-icon"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title">
+                <span class="page-title-icon bg-gradient-primary text-white me-2">
+                  <i class="mdi mdi-home"></i>
+                </span> Dashboard
+              </h3>
+              <nav aria-label="breadcrumb">
+                <ul class="breadcrumb">
+                  <li class="breadcrumb-item active" aria-current="page">
+                    <span></span>Overview <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+           </div>
+        </div>
+
+
+
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div class="container-fluid d-flex justify-content-between">
+              <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright © bootstrapdash.com 2021</span>
+            </div>
+          </footer>
+       
+        </div>
+      </div>
+     
+    
+  </body>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,82 @@
+<template>
+<body>
+  <div>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+                                  <a class="navbar-brand" href="#"><img src="path/to/assets/images/logo.svg" alt="PM sHoP"></a>
+                                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2"
+                                      aria-controls="navbarSupportedContent2" aria-expanded="false" aria-label="Toggle navigation">
+                                      <span class="navbar-toggler-icon"></span>
+                                  </button>
+
+                                  <div class="dropdown">
+                                  
+                                  <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                  <span>STORES</span>
+                                  </button>
+                                  <ul class="dropdown-menu">
+                                  <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                                  <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                                  <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                                  </ul>
+                                  
+                                  </div>
+                              
+                                  <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+                                      <ul class="navbar-nav mr-auto">
+                                          <li class="nav-item active">
+                                              <a class="nav-link" href="#">Home</a>
+                                          </li>
+                                          <li class="nav-item">
+                                              <a class="nav-link" href="#">Calendar</a>
+                                          </li>
+                                          <li class="nav-item">
+                                              <a class="nav-link" href="#">Promos and Events</a>
+                                          </li>
+                                      </ul>
+                                      <ul class="navbar-nav">
+                                          <button class="btn btn-outline-success bg-white mr-2">Details</button>
+                                          
+                                          <button class="btn btn-danger">Contact</button>
+                                      </ul>
+                                  </div>
+                              </nav>
+    <div class="jumbotron">
+      <h1 class="display-4">Welcome to Our Website</h1>
+      <p class="lead">AN ONLINE E-COMMERCE SHOP FOR PUBLIC MARKET</p>
+      
+      
+    </div>
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">KINILAW</h5>
+              <p class="card-text">Description of Feature 1 goes here.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">KILAWIN</h5>
+              <p class="card-text">Description of Feature 2 goes here.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+    <hr class="my-4">
+    <p>Explore our features and services.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+   
+  </div>
+  </body>
+</template>
+
+
